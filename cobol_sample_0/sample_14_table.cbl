9c762cbdbd3ad3f773b08ed51b835c161f908ed7
Oda <kurotamasirenllzz814@gmail.com>
2021-12-26 20:12:48 +0900

    part input

--- cobol_sample_0/sample_14_table.cbl
+++ cobol_sample_0/sample_14_table.cbl
@@ -13,19 +13,21 @@ DATA DIVISION.
             10 POINT PIC 9(3).
 PROCEDURE DIVISION.
     MAIN_PART SECTION.
-        MOVE 1 TO NUM(1).
-        MOVE 2 TO NUM(2).
-        MOVE 3 TO NUM(3).
-        MOVE "TANAKA" TO NAME(1).
-        MOVE "NOZOMI" TO NAME(2).
-        MOVE "SUZUKI" TO NAME(3).
-        MOVE 75 TO POINT(1).
-        MOVE 65 TO POINT(2).
-        MOVE 100 TO POINT(3).
-        DISPLAY "NUMBER INPUT".
-        ACCEPT CONSOLE_INPUT FROM CONSOLE.
-        DISPLAY STUDENT(CONSOLE_INPUT).
-        STOP RUN.
+        INPUT_PART.
+            MOVE 1 TO NUM(1).
+            MOVE 2 TO NUM(2).
+            MOVE 3 TO NUM(3).
+            MOVE "TANAKA" TO NAME(1).
+            MOVE "NOZOMI" TO NAME(2).
+            MOVE "SUZUKI" TO NAME(3).
+            MOVE 75 TO POINT(1).
+            MOVE 65 TO POINT(2).
+            MOVE 100 TO POINT(3).
+        OUTPUT_PART.
+            DISPLAY "NUMBER INPUT".
+            ACCEPT CONSOLE_INPUT FROM CONSOLE.
+            DISPLAY STUDENT(CONSOLE_INPUT).
+            STOP RUN.
 *>3人の生徒情報について、表を作成する
 *>それぞれの生徒のナンバー、名前、ポイントのデータ項目を用意する
 *>データ項目にデータを入力する
